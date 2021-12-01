912e208e2133db8c0f14a78b2ca90470b97aaed1
truongduy12 <duynnt12@outlook.com>
2021-12-01 16:16:51 +0700

    adding style

--- src/App.js
+++ src/App.js
@@ -4,15 +4,31 @@ import './App.css';
 function App() {
   const [time, setTime] = useState(new Date().toLocaleTimeString('vi-VN'));
   const [date, setDate] = useState(new Date().toLocaleDateString('vi-VN'));
+  const [style, setStyle] = useState({
+    backgroundColor: "white",
+    color: "black",
+  });
+
+  const randomColor = () => {
+    let color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return color;
+  }
+
   useEffect(() => {
-    let timer = setInterval(() =>  {
+    let timer = setInterval(() => {
       setTime(new Date().toLocaleTimeString('vi-VN'));
-      setDate(new Date().toLocaleDateString('vi-VN'))
+      setDate(new Date().toLocaleDateString('vi-VN'));
+      setStyle({
+        backgroundColor: randomColor(),
+        color: randomColor(),
+      })
     }, 1000);
-    return () => clearInterval(timer);
+    return () => {
+      clearInterval(timer);
+    };
   });
   return (
-    <div className="wrapper">
+    <div className="wrapper" style={style}>
       <div className="date">{date}</div>
       <div className="time">{time}</div>
     </div>
